1d2f4014556dfd3806899e7ad96dda352b4c426f
BrunoCosta80 <105901224+BrunoCosta80@users.noreply.github.com>
2022-10-13 21:58:08 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+const task = document.querySelector("#tarefa")
+const btnAddTask = document.getElementById("add_tarefa")
+const toDoList = document.querySelector("#a-fazer")
+
+btnAddTask.onclick = function(evento) {
+    evento.preventDefault()
+  const newTask = document.createElement("li")
+  // newTask.innerText = task.value
+  const taskText = document.createTextNode(task.value)
+  newTask.appendChild(taskText)
+  toDoList.appendChild(newTask)  
+}
